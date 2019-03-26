a04030c1a52b433f28614b3de33005bd4810f0bd
Allu <30941996+Alluysios@users.noreply.github.com>
2019-03-25 21:34:50 -0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+    $('.product__slide').slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 4,
+        autoplay: true,
+        autoplaySpeed: 1200,
+        slidesToScroll: 1
+    })
+
+});
